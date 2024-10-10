cd2f1deca2bede5ba080096a4261a713099ac3e6
Rohit kumar yadav <rohitkuyada@gmail.com>
2024-10-11 01:08:29 +0530

    add new middleware

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  const publicRoutes = ["/", "/about", "/contact", "/login", "/signup"];
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  console.log("token", token);
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/create", "/profile", "/dashboard"],
+};
