426ce1b27fa452477fbcbdd66a180bed1f868d9a
Rohit kumar yadav <rohitkuyada@gmail.com>
2024-10-14 20:09:43 +0530

    Refactor metadata generation and update default values

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,17 +19,39 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-// export const metadata: Metadata = {
-//   title: "Blogger App",
-//   description: "A simple blogging app built with Next.js and Prisma ORM which allows users to create, read, update and delete blog posts.",
-// };
-
-// export async function generateMetadata({ title, description }: Metadata) {
-//   return {
-//     title: title,
-//     description: description,
-//   };
-// }
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Blogger App",
+    description: "A simple blogging platform built with Next.js, where users can create, read, update, and delete blog posts.",
+    openGraph: {
+      title: "Blogger App",
+      description: "Join the Blogger App community and start sharing your stories!",
+      url: "https://blogging-one-omega.vercel.app",
+      type: "website",
+      images: [
+        {
+          url: "/default-thumbnail.png",
+          width: 800,
+          height: 600,
+          alt: "Blogger App",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Blogger App",
+      description: "Start blogging with ease using our platform!",
+      images: ["/default-thumbnail.jpg"],
+    },
+    alternates: {
+      canonical: "https://blogging-one-omega.vercel.app",
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+  };
+}
 
 
 export default function RootLayout({
