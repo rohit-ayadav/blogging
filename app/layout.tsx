3a89c59b392bf8a4bf4ccd943fd0ab261df0c465
Rohit kumar yadav <rohitkuyada@gmail.com>
2024-10-16 19:35:16 +0530

    adding adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8778160378200057"
+          crossOrigin="anonymous"></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
