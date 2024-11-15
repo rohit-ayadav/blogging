36e302d5ae61e39a8c3865978176333f4af0ac88
Rohit kumar yadav <rohitkuyada@gmail.com>
2024-11-15 14:14:03 +0530

    Refactor theme API route to simplify session handling and improve response consistency

--- app/api/theme/route.ts
+++ app/api/theme/route.ts
@@ -1,22 +1,10 @@
-// This file contains the route for selecting and fetching the selected theme for the user
-// api/theme
-
 import User from "@/models/users.models";
 import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/utils/db";
-import { getSession } from "next-auth/react";
-import { Session } from "next-auth";
+import { getSessionAtHome } from "@/auth";
 
 await connectDB();
-
-interface CustomSession extends Session {
-  user: {
-    id: string;
-    name?: string | null;
-    email?: string | null;
-    image?: string | null;
-  };
-}
+const session = await getSessionAtHome();
 
 export async function POST(request: NextRequest) {
   const { theme } = await request.json();
@@ -24,7 +12,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(
       {
         message: "Theme is required",
-        success: false,
+        success: false
       },
       { status: 400 }
     );
@@ -34,20 +22,17 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(
       {
         message: "Invalid theme selected",
-        success: false,
+        success: false
       },
       { status: 400 }
     );
   }
 
-  const session = (await getSession({
-    req: { headers: Object.fromEntries(request.headers) },
-  })) as CustomSession | null;
   if (!session) {
     return NextResponse.json(
       {
         message: "You need to be logged in to select your theme",
-        success: false,
+        success: false
       },
       { status: 401 }
     );
@@ -59,20 +44,19 @@ export async function POST(request: NextRequest) {
       return NextResponse.json(
         {
           message: "User not found",
-          success: false,
+          success: false
         },
         { status: 404 }
       );
     }
-    
+
     user.theme = theme;
     await user.save();
 
-    
     return NextResponse.json(
       {
         message: "Theme selected successfully",
-        success: true,
+        success: true
       },
       { status: 200 }
     );
@@ -80,7 +64,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(
       {
         message: "An error occurred while selecting your theme",
-        success: false,
+        success: false
       },
       { status: 500 }
     );
@@ -89,21 +73,18 @@ export async function POST(request: NextRequest) {
   return NextResponse.json(
     {
       message: "Theme selected successfully",
-      success: true,
+      success: true
     },
     { status: 200 }
   );
 }
 
 export async function GET(request: NextRequest) {
-  const session = (await getSession({
-    req: { headers: Object.fromEntries(request.headers) },
-  })) as CustomSession | null;
   if (!session) {
     return NextResponse.json(
       {
         message: "You need to be logged in to view your theme",
-        success: false,
+        success: false
       },
       { status: 401 }
     );
@@ -115,7 +96,7 @@ export async function GET(request: NextRequest) {
       return NextResponse.json(
         {
           message: "User not found",
-          success: false,
+          success: false
         },
         { status: 404 }
       );
@@ -123,7 +104,7 @@ export async function GET(request: NextRequest) {
     return NextResponse.json(
       {
         theme: user.theme,
-        success: true,
+        success: true
       },
       { status: 200 }
     );
@@ -131,7 +112,7 @@ export async function GET(request: NextRequest) {
     return NextResponse.json(
       {
         message: "An error occurred while fetching your theme",
-        success: false,
+        success: false
       },
       { status: 500 }
     );
