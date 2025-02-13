9aae74879d0001c8e19fc5cacef4c8b9fea264e5
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-02-13 10:59:17 +0530

    Remove console log for existing MongoDB connection to clean up output

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
             await mongoose.connect(process.env.MONGO_URI);
             console.log('MongoDB connected successfully');
         } else {
-            console.log('MongoDB already connected');
+            // console.log('MongoDB already connected');
         }
     }
     catch (error) {
