eae9ef23695f7bb240e04c9593c2377b078e9eda
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-02-23 15:35:16 +0530

    Add click handler to logo link to close mobile menu

--- app/_component/navigation/navbar/navbarComponent.tsx
+++ app/_component/navigation/navbar/navbarComponent.tsx
@@ -60,7 +60,8 @@ const DevBloggerNavbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <Link href="/" className="flex items-center space-x-2 shrink-0">
+          <Link href="/" className="flex items-center space-x-2 shrink-0"
+            onClick={() => setIsMobileMenuOpen(false)}>
             <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-indigo-500 to-purple-600 
               flex items-center justify-center shadow-lg">
               <Code size={24} className="text-white" />
